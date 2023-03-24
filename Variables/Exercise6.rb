7b48f0b7493dcb32fa14823d5b5279a91012dc22
Amar Thakor <amarthakor97@gmail.com>
2023-03-24 10:48:55 -0700

    chap 3 exercises

--- Variables/Exercise6.rb
+++ Variables/Exercise6.rb
@@ -8,5 +8,3 @@ end
 
 puts x
 
-
-puts 'The first prints 3 to the screen. The second throws an error undefined local variable or method because x is not available as it is created within the scope of the do/end block.'
