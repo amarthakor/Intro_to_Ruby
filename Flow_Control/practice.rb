cf516919a30a71949d6d59d8bf9b0bf46d4f8df4
Amar Thakor <amarthakor97@gmail.com>
2023-04-26 02:30:23 -0700

    practice problems

--- Flow_Control/practice.rb
+++ Flow_Control/practice.rb
@@ -1,12 +1,14 @@
 # practice file for various exercises
+# Write a while loop that takes input from the user, performs an action, 
+# and only stops when the user types "STOP". Each loop can get info from the user
 
-# perform_again.rb
+a = gets.chomp.to_i
 
-loop do
-  puts "Do you want to do that again?"
-  answer = gets.chomp
-  if answer == 'n'
-    break
+def count_downer(a)
+  if a >= 0
+  puts a
+  count_downer(a-1)
   end
 end
 
+count_downer(a)
